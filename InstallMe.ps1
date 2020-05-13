3bbecb89d741447365a55d30d0e244bb26264e37
Connor <connorjameslow@gmail.com>
2020-05-13 10:22:36 -0700

    [update] improved specificity of install script existing tool check regexp

--- InstallMe.ps1
+++ InstallMe.ps1
@@ -1,6 +1,6 @@
 Set-Location StackOverflow
 dotnet pack -c Release
-if (( @(dotnet tool list -g) -match "stackoverflow*" ).Length -gt 0) {
+if (( @(dotnet tool list -g) -match "^stackoverflow\s*" ).Length -gt 0) {
     dotnet tool update stackoverflow -g
 }
 else {
